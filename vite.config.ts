2ed92d8d4d19113465f555fa3167d25667556f2f
kunallunkad <kunallunkad@gmail.com>
2025-11-24 15:39:44 +0530

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -7,4 +7,14 @@ export default defineConfig({
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
+  server: {
+    host: '0.0.0.0',
+    port: 5000,
+    strictPort: true,
+    hmr: {
+      protocol: 'wss',
+      host: undefined,
+      clientPort: 443,
+    },
+  },
 });
